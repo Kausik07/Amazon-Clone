095b238b45ad02fcdd6e6f92272753e992d13cc9
kausik07 <kausikpattanaik2002@gmail.com>
2023-02-14 19:59:34 +0530

    Added Checkout Page

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyDfZ8C4s95vw38YIVwK62FQdQsasTS2loI",
+  authDomain: "clone-5e002.firebaseapp.com",
+  projectId: "clone-5e002",
+  storageBucket: "clone-5e002.appspot.com",
+  messagingSenderId: "730674396364",
+  appId: "1:730674396364:web:6889ddecdb7827f6bb9669",
+  measurementId: "G-FP01LL8Y0J",
+};
